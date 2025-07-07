869691e21dd2307eb5c328047f11bdc34be5270b
mingdodev <mingdo.dev@gmail.com>
2025-07-07 17:16:04 +0900

    feat: socket.io 서버 생성

--- src/server.js
+++ src/server.js
@@ -1,5 +1,22 @@
+const http = require('http');
+const { Server } = require('socket.io');
+
+const connectDB = require('./db/mongoose');
+const handleSocket = require('./socket');
 const app = require('./app');
+
 const PORT = 3000;
+
+connectDB();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+handleSocket(io);
 
 app.listen(PORT, () => {
     console.log(`Server running at port ${PORT}`);
