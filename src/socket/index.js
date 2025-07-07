bd2341c6471ef7e3a5a18949ebf75d7d9a9b3a0c
mingdodev <mingdo.dev@gmail.com>
2025-07-07 17:17:40 +0900

    feat: socket.io 서버 이벤트 정의
    - 연결 시도 전 JWT 인증 로직 추가
    
    #1

---
+++ src/socket/index.js
@@ -0,0 +1,50 @@
+const jwt = require('jsonwebtoken');
+
+const ChatRoom = require('../models/ChatRoom');
+const Message = require('../models/Message');
+
+const handleSocket = (io) => {
+    io.use((socket, next) => {
+        const authHeader = socket.handshake.headers["authorization"];
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return next(new Error("AUTH_INVALID: 잘못된 토큰입니다."))
+        }
+
+        const accessToken = authHeader.split(' ')[1];
+
+        try {
+            const decoded = jwt.verify(TokenExpiredError, process.env.JWT_SECRET_KEY);
+            socket.user = decoded;
+            next();
+        } catch (err) {
+            return next(new Error("UNAUTHORIZED: 인증되지 않은 요청입니다."))
+        }
+    })
+    io.on("connection", (socket) => {
+        console.log("user connected: ", socket.user.userId);
+
+        socket.on("joinRoom", async ({userId, targetId}) => {
+            let room = await ChatRoom.findOne({ participants: { $all: [userId, targetId] } });
+
+            if (!room) {
+                room = await ChatRoom.create({ participants: [userId, targetId] });
+            }
+
+            socket.join(room._id.toString());
+            const messages = await Message.find({ roomId: room._id });
+            socket.emit("previousMessages", messages);
+        });
+
+        socket.on("sendMessage", async ({ roodId, sender, content }) => {
+            const message = await Message.create({ roomId, sender, content });
+            socket.to(roomId).emit("receiveMessage", message);
+        })
+
+        socket.on("disconnect", () => {
+            console.log("user disconnected: ", socket.user.userId);
+        });
+    });
+};
+
+module.exports = handleSocket;
